60a71a2743b6e3240e9a3610512b45ce10988755
R2J <saharituraj@gmail.com>
2020-04-18 12:42:27 +0530

    testmap.py added

---
+++ testmap.py
@@ -0,0 +1,8 @@
+files ={"java":100, "python":112, "c":11}} 
+key_list = list(files.keys())
+val_list = list(files.values())
+
+print(files.values())
+for ele in val_list:
+  if '.dvf' in ele:
+      print(key_list[val_list.index(ele)])
