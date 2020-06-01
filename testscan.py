df8bc9f1956b49f1d3a228fcd5b140eea9d6fe00
R2J <saharituraj@gmail.com>
2020-06-01 22:17:46 +0530

    testing scan

--- testscan.py
+++ testscan.py
@@ -2,10 +2,10 @@ import os
 from pathlib import Path 
 
 
-for i in os.scandir():     #scans the folder
+for i in os.scandir():     
     path=Path(i)
     print(i,end="->")
-    print(path,end="->")    #read the name of scanned files present in the folder 
+    print(path,end="->")     
     print(path.suffix.lower())   
     print()
     
